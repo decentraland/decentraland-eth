898d957e77c349b74c902795b9660918913eb811
Ignacio Mazzara <nachomazzara@gmail.com>
2018-07-16 12:44:29 -0300

    feat: rename mortgage creator and update ABI (#21)
    MortgageCreator is MortgageHelper now

--- src/contracts/MortgageHelper.ts
+++ src/contracts/MortgageHelper.ts
@@ -1,15 +1,15 @@
 import { Contract } from '../ethereum'
 import { fulfillContractMethods } from './verification'
-const { abi } = require('./artifacts/MortgageCreator.json')
+const { abi } = require('./artifacts/MortgageHelper.json')
 
 /** MortgageCreator contract class */
-export class MortgageCreator extends Contract {
+export class MortgageHelper extends Contract {
   constructor(address: string) {
     super(address, abi)
     fulfillContractMethods(this, abi)
   }
 
   getContractName() {
-    return 'MortgageCreator'
+    return 'MortgageHelper'
   }
 }
