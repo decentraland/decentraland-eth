83175319fa8ad0ad3b5678858f7be2d800b6d141
Ignacio Mazzara <nacho@decentraland.org>
2019-05-17 18:23:23 -0300

    fix: missing export update

--- src/contracts/AvatarNameRegistry.ts
+++ src/contracts/AvatarNameRegistry.ts
@@ -2,7 +2,7 @@ import { Contract } from '../ethereum'
 
 const { abi } = require('./artifacts/AvatarNameRegistry.json')
 
-export class UsernameRegistry extends Contract {
+export class AvatarNameRegistry extends Contract {
   constructor(address: string) {
     super(address, abi)
   }
